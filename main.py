fd336d1114b75d957f9455ad9defeb81a4625184
KuromiChan5432 <mandarinchik5432@yandex.com>
2025-09-30 21:41:43 +0300

    Normolize script

---
+++ main.py
@@ -0,0 +1,10 @@
+from solving_simple_quadratic_equations import solve
+
+if __name__ == '__main__':
+    x_1,x_2 = solve()
+    if x_1 != 0  and x_2 != 0:
+        print(f'Корни уравнения равны {x_1} {x_2}')
+    elif x_1!= 0 and x_2 == 0:
+        print(f'Корень уравнения равен {x_1}')
+    elif x_1 == 0 and x_2 == 0:
+        print('Корней нет')
--- solving_simple_quadratic_equations.py
+++ solving_simple_quadratic_equations.py
@@ -1,35 +1,44 @@
 import math
 
-x1 = 0
-x2 = 0
-newrav = 0
-print('введите коэффициенты для квадратного уравнения')
-print('коэффициент а = ')
-a = int(input())
-print('коэффициент b =')
-b = int(input())
-print('коэффициент c = ')
-c = int(input())
-print('введите число после равно')
-rav = int(input())
-if rav != 0:
-    newrav += rav * (-1)
-    c = c + newrav
-print('После равно есть переменная x? введите ответ "Да"/"Нет"')
-ans = input()
-if ans == 'Да':
-    print('Введите коэффициент переменной')
-    koef = int(input())
-    b = b + koef
-D = b ** 2 - 4 * a * c
-if D > 0:
-    print('Дискриминант больше 0 и равен = ', D)
-    x1 = (-b + (math.sqrt(D))) // (2 * a)
-    x2 = (-b - (math.sqrt(D))) // (2 * a)
-    print('Корни уравнения равны = ', x1, ' ', x2)
-elif D == 0:
-    print('Дискриминант равен нулю')
-    x1 = (-b) / (2 * a)
-    print('Корень уравнения равен = ', x1)
-elif D < 0:
-    print('Дискриминант меньше нуля, следовательно в уравнении нет корней')
+def normalize() -> (int, int, int):
+    a,b,c,e,coef = get_input()
+    if e != 0:
+        c+=e*(-1)
+    b+=coef*(-1)
+    return a,b,c
+
+
+def get_input() -> (int,int,int,int,int):
+    coef = None
+    print('введите коэффициенты для квадратного уравнения')
+    print('коэффициент а = ')
+    a = int(input())
+    print('коэффициент b =')
+    b = int(input())
+    print('коэффициент c = ')
+    c = int(input())
+    print('введите число после равно')
+    e = int(input())
+    print('После равно есть переменная x? введите ответ "Да"/"Нет"')
+    ans = input()
+    if ans == 'Да':
+        print('Введите коэффициент переменной')
+        coef = int(input())
+    return a,b,c,e,coef
+
+
+def solve():
+    x_1 = 0
+    x_2 = 0
+    a,b,c = normalize()
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        print('Дискриминант больше 0 и равен = ', d)
+        x_1 = (-b + (math.sqrt(d))) // (2 * a)
+        x_2 = (-b - (math.sqrt(d))) // (2 * a)
+    elif d == 0:
+        print('Дискриминант равен нулю')
+        x_1 = (-b) / (2 * a)
+    elif d < 0:
+        print('Дискриминант меньше нуля, следовательно в уравнении нет корней')
+    return x_1, x_2
